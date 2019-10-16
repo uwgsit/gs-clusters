45f97d679627722fbc446aa349a73d731e673655
Skylar Thompson <skylar2@uw.edu>
2019-10-16 09:38:38 -0700

    test-job-1 first pass

---
+++ test-job-1.sh
@@ -0,0 +1,17 @@
+#$ -cwd
+#$ -S /bin/bash
+#$ -l mfree=2G,h_rt=65
+
+date
+echo "Job ${JOB_ID} started on ${HOSTNAME}" >&2
+
+echo "Default Python: $(python --version)"
+
+. /etc/profile.d/modules.sh
+module load modules{,-init,-gs} python/3.6.4
+
+echo "Modules Python: $(python --version)"
+
+date
+sleep 60
+echo "Job ${JOB_ID} ended" >&2

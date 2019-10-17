90ab270a86f44c484f066df3036c3d08af151cad
Skylar Thompson <skylar2@uw.edu>
2019-10-17 08:27:12 -0700

    reduce runtime, remove comment

--- test-reserve-job-1.sh
+++ test-reserve-job-1.sh
@@ -1,17 +1,17 @@
-# CHANGE FOR CLASS
 #$ -R y
 #$ -P sage
 #$ -cwd
 #$ -S /bin/bash
-#$ -l mfree=50G,h_rt=65
+#$ -l mfree=50G,h_rt=35
 #$ -o ./nobackup/sgeoutput
 #$ -e ./nobackup/sgeoutput
+#$ -t 1-10
 
 set -e
 
 date
 echo "Job ${JOB_ID} started on ${HOSTNAME} with ${NSLOTS} slots" >&2
 
-sleep 60
+sleep 30
 date
 echo "Job ${JOB_ID} ended" >&2

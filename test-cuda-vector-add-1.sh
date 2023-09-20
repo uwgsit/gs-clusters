9f7b7e8ee787183e97f2641a9d2533fbc866f1e3
Skylar Thompson <skylar2@uw.edu>
2023-09-20 16:17:14 -0700

    vector_add submit scripts

---
+++ test-cuda-vector-add-1.sh
@@ -0,0 +1,25 @@
+#$ -P sage
+#$ -cwd
+#$ -S /bin/bash
+#$ -l mfree=32G,h_rt=0:5:0,cuda=1
+#$ -R y
+#$ -o ./nobackup/sgeoutput
+#$ -e ./nobackup/sgeoutput
+
+set -e
+
+# Use older CUDA for compatibility with older GPUs
+module load cuda/10.0
+
+make vector_add_cuda
+
+VECTOR_SIZE="$1"
+BLOCK_COUNT="$2"
+THREAD_COUNT="$3"
+
+if [[ "x${VECTOR_SIZE}" = "x" || "x${BLOCK_COUNT}" = "x" || "x${THREAD_COUNT}" = "x" ]]; then
+    echo "Supply vector size, block count, and thread count" >&2
+    exit 1
+fi
+
+time ./vector_add_cuda -n "${VECTOR_SIZE}" -b "${BLOCK_COUNT}" -t "${THREAD_COUNT}"
---
+++ test-serial-vector-add-1.sh
@@ -0,0 +1,18 @@
+#$ -P sage
+#$ -cwd
+#$ -S /bin/bash
+#$ -l mfree=32G,h_rt=0:5:0
+#$ -o ./nobackup/sgeoutput
+#$ -e ./nobackup/sgeoutput
+
+set -e
+
+make vector_add
+
+VECTOR_SIZE="$1"
+if [[ "x${VECTOR_SIZE}" = "x" ]]; then
+    echo "Supply vector size" >&2
+    exit 1
+fi
+
+time ./vector_add -n "${VECTOR_SIZE}"
